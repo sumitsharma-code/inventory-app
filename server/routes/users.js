5db8687675427cb73206cc569d585cc427e95368
sumitsharma-code <97402908+sumitsharma-code@users.noreply.github.com>
2025-12-14 14:27:27 +0530

    fix: remove duplicate User model

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,14 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const userController = require('../controllers/userController');
-// const auth = require('../middleware/auth');
-// const permit = require('../middleware/roles');
-
-// router.get('/', auth, permit('admin'), userController.listUsers);
-// router.delete('/:id', auth, permit('admin'), userController.deleteUser);
-
-// module.exports = router;
-
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
